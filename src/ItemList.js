086048747dda8cbbe5a94ae84c5c005e98e904c8
Jaeyoung Kim <j.jaeykim@gmail.com>
2023-02-04 15:48:41 -0500

    mui added

--- src/ItemList.js
+++ src/ItemList.js
@@ -1,26 +1,43 @@
 import React from "react";
 
-function ItemList({ items }) {
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+} from "@mui/material";
+
+export default function ItemList({ items }) {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-          <th>Color</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map((item, index) => (
-          <tr key={index}>
-            <td>{item.name}</td>
-            <td>{item.price}</td>
-            <td>{item.color}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Color</TableCell>
+            <TableCell align="right">Sold</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {items.map((item, index) => (
+            <TableRow key={index}>
+              <TableCell component="th" scope="row">
+                {item.name}
+              </TableCell>
+              <TableCell align="right">{item.price}</TableCell>
+              <TableCell align="right">{item.color}</TableCell>
+              <TableCell align="right">
+                <Button>Sold</Button>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
-
-export default ItemList;
